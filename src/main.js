7c21d4478d00d52807bbff2d1e04dfc69f247c19
shi580927-blip <shi580927@gmail.com>
2026-09-20 18:59:02 +0200

    Add Phaser test bootstrap

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import TestFishAtlasScene from './scenes/TestFishAtlasScene.js';
+
+const config = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 1000,
+  height: 720,
+  backgroundColor: '#06283a',
+  transparent: false,
+  antialias: true,
+  pixelArt: false,
+  roundPixels: false,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: [TestFishAtlasScene]
+};
+
+const game = new Phaser.Game(config);
+
+window.game = game;
